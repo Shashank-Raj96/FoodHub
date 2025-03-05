c1dc331a1cbad5f78d43696e8e5ec0151441201f
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-03-05 16:13:01 +0530

    Adding a User.js functional component to About us

--- src/component/About.js
+++ src/component/About.js
@@ -1,8 +1,11 @@
+import User from "./User";
+
 const About = () => {
     return (
         <div>
         <h1>About</h1>
         <h2> This is Food Hub application</h2>
+        <User />
         </div>
     );
     };
---
+++ src/component/User.js
@@ -0,0 +1,8 @@
+const User = () => {
+    return <div className="user-card">
+        <h2> Name : Shashank </h2>
+        <h3> Location : Hajipur </h3>
+    </div>
+}
+
+export default User;
