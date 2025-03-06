ce27c2337ae64fcb9826358df287971ffd390bf9
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-03-06 15:27:45 +0530

    Adding multiple State Variable

--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -4,17 +4,20 @@ class UserClass extends React.Component {
 
     constructor (props){
         super(props);
+
         this.state = {
             count : 0,
+            count2 : 2,
         };
     }
     render () {
         
         const {name , location } = this.props;
-        const {count}= this.state;
+        const {count,count2}= this.state;
         return  (
              <div className="user-card">
             <h1>Count : {count}</h1>
+            <h1>Count 2 : {count2}</h1>
             <h2> Name : {name} </h2>
             <h3> Location : {location} </h3>
         </div>
