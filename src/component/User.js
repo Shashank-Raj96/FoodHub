83199f456f209a8f9b60536b1fa5aa920ad6709a
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-03-06 15:17:18 +0530

    creating a State Variable in class component using this

--- src/component/User.js
+++ src/component/User.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
-const User = (props) => {
+const User = ( props ) => {
     const [count] = useState(0);
+    const [count2] = useState(1);
     return <div className="user-card">
-        <h1>Count = {count}</h1>
+        <h1>Count 2 = {props.count2}</h1>
+        <h1>Count = {props.count}</h1>
         <h2> Name : {props.name} </h2>
         <h3> Location : Hajipur </h3>
     </div>
