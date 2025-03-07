e0493a6a1cf3ef7655c9b855a07bc79efe76b87a
Shashank-Raj96 <shashankraj0915@gmail.com>
2025-03-07 19:05:51 +0530

    Using lazy( ) seperating bundle for the Grocery component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {lazy, Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./component/Header";
 import Body from "./component/Body"; 
@@ -8,7 +8,17 @@ import Error from "./component/Error";
 import RestaurantMenu from "./component/RestaurantMenu";
 import { createBrowserRouter , RouterProvider ,Outlet} from "react-router";
 import RestaurantMenu from "./component/RestaurantMenu";
+//import Grocery from "./component/Grocery";
+
+// Chunking 
+// Code Splitting 
+// Dynamic Bundling
+// Lazy Laoding
+// On Demand Loading 
+//(These all are same terms which means chunks your Application . Make your application into smaller Chunks)
  
+const Grocery = lazy (() => import("./component/Grocery") );
+
 const AppLayout =() => {
   return (
     <div className="app">
@@ -36,6 +46,11 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact/>
+      },
+
+      {
+        path: "/grocery",
+        element:<Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense> 
       },
 
       {
---
+++ src/component/Grocery.js
@@ -0,0 +1,10 @@
+const Grocery = () => {
+    return(
+        <h1>
+            {""}
+            Our Grocery Online Store , and we have a list of child component inside this Web Page !!!
+        </h1>
+    );
+};
+
+export default Grocery;
