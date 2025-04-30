4dced941d353171efef61d13c00ed6aef83f4324
snehil <snehil@snehils-MacBook-Air.local>
2025-04-30 21:15:49 +0530

    photo

--- src/component/RestaurantCard.js
+++ src/component/RestaurantCard.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import {CDN_URL} from "../../utils/constants";
 import UserContext from "../../utils/UserContext";
+import { imagesUrl } from "../Images/ImageUrl";
 
 const RestaurantCard = (props) => {
     const { resData } = props;
@@ -8,16 +9,22 @@ const RestaurantCard = (props) => {
   
  
   const {loggedInUser} = useContext(UserContext)
-  
+  console.log("info",info)
     //console.log(info.name,info.cloudinaryImageId);
+    function getRandomUrl() {
+      const index = Math.floor(Math.random() * imagesUrl.length);
+      return imagesUrl[index];
+    }
     return(
-      <div className="m-4 p-4 w-[200px] rounded-lg bg-gray-100 hover:bg-gray-200" 
+      <div className="m-4 p-4 w-[200px] rounded-lg bg-gray-100 hover:bg-gray-200 " 
       
       >
              <img
               className="rounded-lg"
               alt="res-logo"
-              src={CDN_URL + info.cloudinaryImageId}
+              
+              src={getRandomUrl()}
+              
               /> 
   
              <h3 className="font-bold py-3 text-lg">{info.name}</h3>
